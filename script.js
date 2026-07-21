df4c578beceec1d9cda6a6932a6346e92f707a88
Noran Fawzy <noranfawzy05@gmail.com>
2026-07-22 01:47:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Array of image URLs
+let images = [
+    "../images/image copy 2.png",
+    "../images/image copy.png",
+    "../images/image.png"
+];
+
+// Get the image element
+let slider = document.getElementById("slider");
+
+// Start with the first image
+let index = 0;
+slider.src = images[index];
+
+// Change the image every second
+setInterval(function () {
+    index++;
+    if (index === images.length) {
+        index = 0;
+    }
+    slider.src = images[index];
+}, 1000);
